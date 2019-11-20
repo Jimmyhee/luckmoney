7b27be85a41ff1ba6d50b7f5c691105bc982dd29
Jimmyhee <nepuhzm@163.com>
2019-11-20 11:28:33 +0800

    first commit

---
+++ CodeCenter/src/main/java/com/hzm/codecenter/CodecenterApplication.java
@@ -0,0 +1,13 @@
+package com.hzm.codecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecenterApplication.class, args);
+    }
+
+}
